7d843638ae28660dbad539d99a1da4ddd91c98b3
unknown <bangkay>
2015-10-15 01:44:11 +0800

    updated db

--- Database/evaluation.sql
+++ Database/evaluation.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 14, 2015 at 06:12 PM
+-- Generation Time: Oct 14, 2015 at 07:42 PM
 -- Server version: 5.6.26
 -- PHP Version: 5.5.28
 
@@ -23,78 +23,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `department`
+-- Table structure for table `result`
 --
 
-CREATE TABLE IF NOT EXISTS `department` (
+CREATE TABLE IF NOT EXISTS `result` (
+  `R_ID` int(11) NOT NULL,
   `D_ID` int(11) NOT NULL,
-  `D_name` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `faculty`
---
-
-CREATE TABLE IF NOT EXISTS `faculty` (
-  `F_ID` int(11) NOT NULL,
-  `F_name` text NOT NULL,
-  `L_name` text NOT NULL,
-  `D_ID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `question`
---
-
-CREATE TABLE IF NOT EXISTS `question` (
-  `Q_ID` int(11) NOT NULL,
-  `Q_name` text NOT NULL,
-  `Description` text NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `question`
---
-
-INSERT INTO `question` (`Q_ID`, `Q_name`, `Description`) VALUES
-(1, 'Observes neatness and proper grooming.', 'Personality'),
-(2, 'Uses Appropriate Language.', 'Personality'),
-(3, 'Shows enthusiasm(Lively, energetic).', 'Personality'),
-(4, 'Project Self-Confidence', 'Personality'),
-(5, 'Respect Students', 'Personality'),
-(6, 'State the lesson objectives clearly', 'Teaching Procedure'),
-(7, 'Organizes the Lesson well', 'Teaching Procedure'),
-(8, 'Present the lesson clearly', 'Teaching Procedure'),
-(9, 'Encourage the student to express opinions and ideas.', 'Teaching Procedure'),
-(10, 'integrate values appropriately in the lesson', 'Teaching Procedure');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `semester`
---
-
-CREATE TABLE IF NOT EXISTS `semester` (
   `Sem_ID` int(11) NOT NULL,
-  `Sem_name` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `subject`
---
-
-CREATE TABLE IF NOT EXISTS `subject` (
+  `F_ID` int(11) NOT NULL,
   `S_ID` int(11) NOT NULL,
-  `S_name` text NOT NULL,
-  `Sem_ID` int(11) NOT NULL,
-  `F_ID` int(11) NOT NULL,
-  `D_ID` int(11) NOT NULL
+  `ans1` int(11) NOT NULL,
+  `ans2` int(11) NOT NULL,
+  `ans3` int(11) NOT NULL,
+  `ans4` int(11) NOT NULL,
+  `ans5` int(11) NOT NULL,
+  `ans6` int(11) NOT NULL,
+  `ans7` int(11) NOT NULL,
+  `ans8` int(11) NOT NULL,
+  `ans9` int(11) NOT NULL,
+  `ans10` int(11) NOT NULL,
+  `Remarks` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -102,64 +50,20 @@ CREATE TABLE IF NOT EXISTS `subject` (
 --
 
 --
--- Indexes for table `department`
+-- Indexes for table `result`
 --
-ALTER TABLE `department`
-  ADD PRIMARY KEY (`D_ID`);
-
---
--- Indexes for table `faculty`
---
-ALTER TABLE `faculty`
-  ADD PRIMARY KEY (`F_ID`);
-
---
--- Indexes for table `question`
---
-ALTER TABLE `question`
-  ADD PRIMARY KEY (`Q_ID`);
-
---
--- Indexes for table `semester`
---
-ALTER TABLE `semester`
-  ADD PRIMARY KEY (`Sem_ID`);
-
---
--- Indexes for table `subject`
---
-ALTER TABLE `subject`
-  ADD PRIMARY KEY (`S_ID`);
+ALTER TABLE `result`
+  ADD PRIMARY KEY (`R_ID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `department`
+-- AUTO_INCREMENT for table `result`
 --
-ALTER TABLE `department`
-  MODIFY `D_ID` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `faculty`
---
-ALTER TABLE `faculty`
-  MODIFY `F_ID` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `question`
---
-ALTER TABLE `question`
-  MODIFY `Q_ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
---
--- AUTO_INCREMENT for table `semester`
---
-ALTER TABLE `semester`
-  MODIFY `Sem_ID` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `subject`
---
-ALTER TABLE `subject`
-  MODIFY `S_ID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `result`
+  MODIFY `R_ID` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
